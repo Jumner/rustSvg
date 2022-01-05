ddc1a06fb488f3ac6a94a886bd773019e3a9fbd0
JumnerArch <1frankjus@hdsb.ca>
2022-01-05 18:18:06 -0500

    now its clean

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,27 @@
-use image::pnm::PnmDecoder;
-use image::ImageFormat;
-use ndarray_vision::core::*;
-use ndarray_vision::format::netpbm::*;
-use ndarray_vision::format::*;
-use ndarray_vision::processing::*;
 use std::env;
 use std::env::current_exe;
-use std::fs::File;
 use std::path::PathBuf;
+
+use image::DynamicImage;
 
 extern crate image;
 
 fn main() {
-	println!("Program started!");
 	let directory = match parse_args() {
 		Err(msg) => panic!("{:?}", msg),
 		Ok(directory) => directory,
 	};
-	println!("Directory {:?}", directory);
 
 	if let Ok(mut root) = current_exe() {
 		root.pop();
 		root.pop();
 		root.pop();
-		let mut cameraman = PathBuf::from(&root);
-		cameraman.push(directory);
+		let mut path = PathBuf::from(&root);
+		path.push(directory);
 
-		let img = image::open(cameraman).expect("Error opening file");
-		let mut output = File::create(&format!("tmp.jpg")).expect("tmp File creation failed");
-		img
-			.write_to(&mut output, ImageFormat::Pnm)
-			.expect("tmp write failed");
-
-		let decoder = PpmDecoder::default();
-		// let de = PnmDecoder::new(read)
-		let image: Image<u8, _> = decoder.decode_file("tmp.ppm").expect("Could not open file");
-		let image: Image<f64, _> = image.into_type();
-		let image: Image<f64, Gray> = image.into();
-
-		let canny = canny_edge(&image);
-
-		let ppm = PpmEncoder::new_plaintext_encoder();
-
-		let mut cameraman = PathBuf::from(&root);
-		cameraman.push("canny.ppm");
-		ppm
-			.encode_file(&canny.into(), cameraman)
-			.expect("Error encoding file");
+		let image = image::open(path).expect("Image open failed").to_luma8();
+		let detection = edge_detection::canny(image, 1.2, 0.2, 0.01);
+		save(detection.as_image(), "canny.png");
 	}
 }
 
@@ -59,12 +34,7 @@ fn parse_args() -> Result<String, String> {
 	return Ok(env::args().into_iter().last().unwrap());
 }
 
-fn canny_edge(img: &Image<f64, Gray>) -> Image<f64, Gray> {
-	let x = CannyBuilder::<f64>::new()
-		.lower_threshold(0.3)
-		.upper_threshold(0.5)
-		.blur((5, 5), [0.4, 0.4])
-		.build();
-	let res = img.canny_edge_detector(x).expect("Canny failed");
-	Image::from_data(res.data.mapv(|x| if x { 1.0 } else { 0.0 }))
+fn save(img: DynamicImage, path: &str) {
+	img.save_with_format(path, image::ImageFormat::Png).unwrap();
+	println!("Saved {:?}", path);
 }
