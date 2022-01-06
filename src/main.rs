5625d467796111fd4af124c4b5558aba6c150e61
JumnerArch <1frankjus@hdsb.ca>
2022-01-06 13:32:43 -0500

    created datatypes

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 		Ok(directory) => directory,
 	};
 
+	let mut normals = Normals::new();
 	if let Ok(mut root) = current_exe() {
 		root.pop();
 		root.pop();
@@ -28,6 +29,8 @@ fn main() {
 				let edge = detection.interpolate(x as f32, y as f32);
 				if edge.magnitude() > 0.0 {
 					println!("{:?}", edge.dir_norm());
+					let my_edge = MyEdge::new(x, y, edge.dir_norm().0, edge.dir_norm().1);
+					normals.push(my_edge);
 				}
 			}
 		}
@@ -46,4 +49,31 @@ fn parse_args() -> Result<String, String> {
 fn save(img: DynamicImage, path: &str) {
 	img.save_with_format(path, image::ImageFormat::Png).unwrap();
 	println!("Saved {:?}", path);
+}
+
+struct MyEdge {
+	position: (usize, usize),
+	direction: (f32, f32),
+}
+
+impl MyEdge {
+	pub fn new(x: usize, y: usize, dx: f32, dy: f32) -> Self {
+		Self {
+			position: (x, y),
+			direction: (dx, dy),
+		}
+	}
+}
+
+struct Normals {
+	list: Vec<MyEdge>,
+}
+
+impl Normals {
+	pub fn new() -> Self {
+		Self { list: vec![] }
+	}
+	pub fn push(&mut self, item: MyEdge) {
+		self.list.push(item);
+	}
 }
